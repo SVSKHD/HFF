4912f518b6e696c1b1a359951f2a18fc6641444e
SVSKHD <8svskhd@gmail.com>
2024-12-12 14:06:01 +0530

    format

--- test/logic.py
+++ test/logic.py
@@ -146,7 +146,6 @@ if __name__ == "__main__":
     start_price = 1.0000
     test_prices = [1.0000, 0.9990, 0.9982, 0.9993]
 
-
     for p in test_prices:
         result, state = decide_trade(eur, start_price, p)
         print("Current Price:", p)
